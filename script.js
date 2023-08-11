e0af7d1c5d04b75733fd680ef748728721742aec
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-11 15:54:49 +0200

    Refactor button click event listeners for improved code structure

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-const peopleInSpace = document.querySelector("[data-js='people-in-space']");
-const listOfPeople = document.querySelector("[data-js='list-of-people']");
-const allButton = document.querySelector("[data-js='all-button']");
-const issButton = document.querySelector("[data-js='iss-button']");
-const tiangongButton = document.querySelector("[data-js='tiangong-button']");
+const peopleInSpaceElement = document.querySelector(
+  "[data-js='people-in-space']"
+);
+const listOfPeopleElement = document.querySelector(
+  "[data-js='list-of-people']"
+);
+const allButtonElement = document.querySelector("[data-js='all-button']");
+const issButtonElement = document.querySelector("[data-js='iss-button']");
+const tiangongButtonElement = document.querySelector(
+  "[data-js='tiangong-button']"
+);
 
-// function to creating list on Dom
+// Create list on the DOM
 function updatePeopleList(peopleArray) {
+  listOfPeopleElement.innerHTML = "";
+
   peopleArray.forEach((person) => {
     const listElement = document.createElement("li");
     listElement.textContent = person.name;
-    listOfPeople.appendChild(listElement);
-    peopleInSpace.textContent = peopleArray.length;
+    listOfPeopleElement.appendChild(listElement);
   });
+
+  peopleInSpaceElement.textContent = peopleArray.length;
 }
 
-async function getPeopleInSpace() {
+// Fetch data
+async function fetchPeopleInSpace() {
   const response = await fetch("http://api.open-notify.org/astros.json");
-  const data = await response.json();
-
-  updatePeopleList(data.people);
-
-  issButton.addEventListener("click", () => {
-    const issPeople = data.people.filter((person) => person.craft === "ISS");
-    listOfPeople.innerHTML = "";
-    updatePeopleList(issPeople);
-  });
-
-  tiangongButton.addEventListener("click", () => {
-    const tiangongPeople = data.people.filter(
-      (person) => person.craft === "Tiangong"
-    );
-    listOfPeople.innerHTML = "";
-    updatePeopleList(tiangongPeople);
-  });
-
-  allButton.addEventListener("click", () => {
-    listOfPeople.innerHTML = "";
-    updatePeopleList(data.people);
-  });
+  const { people } = await response.json();
+  return people;
 }
 
-getPeopleInSpace();
+// Event handler function
+function handleButtonClick(data, craftType) {
+  const filteredPeople = craftType
+    ? data.filter((person) => person.craft === craftType)
+    : data;
+  updatePeopleList(filteredPeople);
+}
+
+// Attach event listeners using "IIFE"
+(async function () {
+  const peopleData = await fetchPeopleInSpace();
+
+  issButtonElement.addEventListener("click", () =>
+    handleButtonClick(peopleData, "ISS")
+  );
+  tiangongButtonElement.addEventListener("click", () =>
+    handleButtonClick(peopleData, "Tiangong")
+  );
+  allButtonElement.addEventListener("click", () =>
+    handleButtonClick(peopleData)
+  );
+
+  // Initial population of people list
+  updatePeopleList(peopleData);
+})();
